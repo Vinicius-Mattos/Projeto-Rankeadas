ef17f33d007b7258c56aae069f9de926ae973a76
ViniciusBarbosa <vinicius.barbosa2@outlook.com>
2023-12-20 20:04:36 -0300

    Update rankeadas.js

--- rankeadas.js
+++ rankeadas.js
@@ -1,14 +1,25 @@
-let vitorias = 50;
-let derrotas = 20;
-let saldo = vitorias - derrotas;
-let nivel
+let saldo = calcularRank(120, 56)
+let nivel = 0
 
 function calcularRank(vitorias, derrotas) {
-    if(vitorias <= 10){
-        nivel = "Ferro"
-    } if else(saldo >= 11 && saldo <= 20) {
-        nivel = "Bronze"
-    }
+    let resultado = vitorias - derrotas
+    return resultado
 }
+
+if(calcularRank <= 10){
+    nivel = "Ferro"
+} else if(calcularRank >= 11 && calcularRank <= 20) {
+    nivel = "Bronze"
+} else if(calcularRank >= 21 && calcularRank <= 50) {
+    nivel = "Prata"
+} else if(calcularRank >= 51 && calcularRank <= 80) {
+    nivel = "Ouro"
+} else if(calcularRank >= 81 && calcularRank <= 90) {
+    nivel = "Diamante"
+} else if(calcularRank >= 91 && calcularRank <= 100) {
+    nivel = "Lendário"
+} else if(calcularRank > 100) {
+    nivel = "Imortal"
+} 
 
 console.log("O Herói tem saldo de " + saldo + " e está no nível de " + nivel)
