b148081521e4432e9efa2bc14dd05cdc57609284
ViniciusBarbosa <vinicius.barbosa2@outlook.com>
2023-12-20 20:55:39 -0300

    Update rankeadas.js
    projeto pronto e testado

--- rankeadas.js
+++ rankeadas.js
@@ -1,25 +1,29 @@
-let saldo = calcularRank(120, 56)
-let nivel = 0
+let saldo = calcularRank(100, 56)
+let nivel = ""
+
+
+
+if(saldo <= 10){
+    nivel = "Ferro"
+} else if(saldo >= 11 && saldo <= 20) {
+    nivel = "Bronze"
+} else if(saldo >= 21 && saldo <= 50) {
+    nivel = "Prata"
+} else if(saldo >= 51 && saldo <= 80) {
+    nivel = "Ouro"
+} else if(saldo >= 81 && saldo <= 90) {
+    nivel = "Diamante"
+} else if(saldo >= 91 && saldo <= 100) {
+    nivel = "Lendário"
+} else if(saldo > 100) {
+    nivel = "Imortal"
+} 
+
+
+
+console.log("O Herói tem saldo de " + saldo + " e está no nível de " + nivel)
 
 function calcularRank(vitorias, derrotas) {
     let resultado = vitorias - derrotas
     return resultado
 }
-
-if(calcularRank <= 10){
-    nivel = "Ferro"
-} else if(calcularRank >= 11 && calcularRank <= 20) {
-    nivel = "Bronze"
-} else if(calcularRank >= 21 && calcularRank <= 50) {
-    nivel = "Prata"
-} else if(calcularRank >= 51 && calcularRank <= 80) {
-    nivel = "Ouro"
-} else if(calcularRank >= 81 && calcularRank <= 90) {
-    nivel = "Diamante"
-} else if(calcularRank >= 91 && calcularRank <= 100) {
-    nivel = "Lendário"
-} else if(calcularRank > 100) {
-    nivel = "Imortal"
-} 
-
-console.log("O Herói tem saldo de " + saldo + " e está no nível de " + nivel)
